1c87b5936b10350d3ec5026f8b56b783c8ce9e88
qfdk <qfdk2010@gmail.com>
2022-05-07 22:02:46 +0200

    fix: 新界面

--- routes/admin.js
+++ routes/admin.js
@@ -3,24 +3,49 @@ const router = express.Router();
 const Jail = require('fail2ban').Jail;
 const Fail2Ban = require('fail2ban').Fail2Ban;
 const f2bSocket = '/var/run/fail2ban/fail2ban.sock';
+const geoip = require('fast-geoip');
 
-const jail = new Jail('sshd', f2bSocket);
 const fail = new Fail2Ban(f2bSocket);
 
 router.get('/', async (req, res, next) => {
-    res.render("admin/index", {...await jail.status, ...await fail.status});
+    res.render("admin/index", { ...await fail.status });
 });
 
-router.get('/unban', async (req, res, next) => {
-    const {ip} = req.query;
+router.get('/jails/:jailname', async (req, res, next) => {
+    const jail = new Jail(req.params.jailname, f2bSocket);
+    const status = await jail.status;
+    if (status) {
+        const ips = status.actions.bannedIPList;
+        for (const ip of ips) {
+            const geo = await geoip.lookup(ip);
+            if (status["info"]) {
+                status["info"].push({
+                    ip,
+                    ...geo
+                });
+            } else {
+                status["info"] = [{
+                    ip,
+                    ...geo
+                }];
+            };
+        }
+    }
+    res.render('admin/jasil', { jailname: req.params.jailname, ...status });
+});
+
+router.get('/jails/:jailname/unban', async (req, res, next) => {
+    const jail = new Jail(req.params.jailname, f2bSocket);
+    const { ip } = req.query;
     await jail.unban(ip);
-    res.redirect('/admin');
+    res.redirect(`/admin/jails/${req.params.jailname}`);
 });
 
-router.post('/ban', async (req, res, next) => {
-    const {ip} = req.body;
+router.post('/jails/:jailname/ban', async (req, res, next) => {
+    const jail = new Jail(req.params.jailname, f2bSocket);
+    const { ip } = req.body;
     await jail.ban(ip);
-    res.redirect('/admin');
+    res.redirect(`/admin/jails/${req.params.jailname}`);
 });
 
 module.exports = router;
