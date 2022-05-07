1b5f4114fbccdf57ceb58e0273c1fd0d3aec409a
qfdk <qfdk2010@gmail.com>
2022-05-07 08:50:13 +0200

    fix : 添加pm2.json

--- routes/index.js
+++ routes/index.js
@@ -16,11 +16,9 @@ router.post('/loginAction', async (req, res, next) => {
     const realPassword = process.env.password || 'admin';
     const realUsername = process.env.username || 'admin';
     if (username === realUsername && password === realPassword) {
-        console.log("登录成功", username, password);
         req.session['login'] = true;
         res.redirect('/admin');
     } else {
-        console.log("登录失败", username, password);
         res.redirect('/');
     }
 });
