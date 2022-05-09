31136c103fcc463da5adc4101767c998ec7a52a6
qfdk <qfdk2010@gmail.com>
2022-05-09 09:05:33 +0200

    fix: 添加规则界面删除

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,9 +2,8 @@ const auth = (req, res, next) => {
     if (req.session.login === true) {
         return next();
     } else {
-        console.log("[鉴权路由] 未登录");
         return res.redirect('/');
     }
 };
 
-module.exports = {auth};
+module.exports = { auth };
--- routes/admin.js
+++ routes/admin.js
@@ -7,22 +7,37 @@ const f2bSocket = '/var/run/fail2ban/fail2ban.sock';
 const geoip = require('fast-geoip');
 const fs = require('fs');
 const JAIL_CONFIG_PATH = `/etc/fail2ban/jail.d`;
+const FILTER_CONFIG_PATH = `/etc/fail2ban/filter.d`;
 const fail = new Fail2Ban(f2bSocket);
 
 router.get('/', async (req, res, next) => {
-    res.render("admin/index", { ...await fail.status });
+    const status = await fail.status;
+    res.render("admin/index", { ...status });
 });
 
 router.get('/jail/add', async (req, res, next) => {
-    res.render(`admin/jail/add`);
+    let filters;
+    fs.readdir(FILTER_CONFIG_PATH, (err, files) => {
+        if (err) return res.send("ERROR");
+        filters = files.map(f => f.split(".conf")[0]);
+        res.render(`admin/jail/add`, { filters });
+    });
 });
 
 router.post('/jail/doAdd', async (req, res, next) => {
-    const { configFileName, content } = req.body;
-    fs.writeFile(`${JAIL_CONFIG_PATH}/${configFileName}.conf`, content, async (err) => {
+    const { jailname, enabled, bantime, maxretry, filter } = req.body;
+    const content = `[${jailname}]
+enabled = ${enabled === 'true'}
+bantime = ${bantime}
+maxretry = ${maxretry}
+filter = ${filter}
+`;
+    fs.writeFile(`${JAIL_CONFIG_PATH}/customConfig.conf`, content, { flag: 'a' }, async (err) => {
         if (err) return res.json(err);
         exec("fail2ban-client reload", async (err) => {
-            if (err) return res.json(err);
+            if (err) {
+                console.log(err);
+            }
             res.redirect('/admin');
         });
     });
@@ -97,6 +112,26 @@ router.post('/jails/:jailname/doEdit', async (req, res, next) => {
             if (err) return res.json(err);
             res.redirect('/admin');
         });
+    });
+});
+
+router.get('/jails/:jailname/delete', async (req, res, next) => {
+    fs.readdir(JAIL_CONFIG_PATH, (err, files) => {
+        if (err) return res.send("ERROR");
+        for (const file of files) {
+            const filePath = `${JAIL_CONFIG_PATH}/${file}`;
+            const content = fs.readFileSync(filePath, "utf-8");
+            if (content.includes(`[${req.params.jailname}]`)) {
+                // 删除配置文件并重新载入client
+                fs.unlink(filePath, err => {
+                    if (err) return res.json(err);
+                    exec("fail2ban-client reload", async (err) => {
+                        if (err) return res.json(err);
+                        res.redirect('/admin');
+                    });
+                })
+            }
+        }
     });
 });
 module.exports = router;
